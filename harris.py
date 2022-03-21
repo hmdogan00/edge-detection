257232ce88def5ad082397a667a0f68196414ea6
hmdogan00 <58589080+hmdogan00@users.noreply.github.com>
2022-03-21 23:08:39 +0300

    Harris Corner Detection Implementation

--- harris.py
+++ harris.py
@@ -1,4 +1,62 @@
 import numpy as np
 
-def harris(img):
-  return []
+def harris(img : np.ndarray, k : int, window_size : int):
+  """Calculates Harris Corner Detection array for every element (pixel) in the given ndarray (image)
+
+  Harris Detector: R = det(H) - k(trace(H))**2
+
+  where trace(H) = eigval_1 + eigval_2
+  and det(H) = eigval_1 * eigval_2
+
+  the eigenvalues are of H's eigenvalues.
+
+  Parameters
+  ----------
+  img : ndarray
+    Image to get its corners calculated
+  
+  k : int
+    k value to weight the trace
+  """
+  # initialize output array and offset
+  output = np.zeros(img.shape)
+  offset = int(window_size / 2)
+
+  # calculate the max range of the pixels
+  y_range, x_range = img.shape
+  y_range -= offset
+  x_range -= offset
+
+  # calculate the gradient of all pixels
+  dy, dx = np.gradient(img)
+  Ixx = dx * dx
+  Ixy = dx * dy
+  Iyy = dy * dy
+
+  for y in range(offset, y_range):
+    for x in range(offset, x_range):
+
+      # limits of the window
+      y_start = y - offset
+      x_start = x - offset
+      y_end = y + offset + 1
+      x_end = x + offset + 1
+
+      # calculate values of H, which is [[I_xx, I_xy], [I_yx, I_yy]]
+      i_xx = Ixx[y_start : y_end, x_start : x_end]
+      i_xy = Ixy[y_start : y_end, x_start : x_end]
+      i_yy = Iyy[y_start : y_end, x_start : x_end]
+
+      # summation to find the intensities
+      s_xx = np.sum(i_xx)
+      s_xy = np.sum(i_xy)
+      s_yy = np.sum(i_yy)
+
+      # compute determinant and trace of H
+      det = (s_xx * s_yy) - (s_xy * s_xy)
+      trace = s_xx + s_yy
+
+      # put the R value of the pixel
+      output[y,x] = det - k * trace**2
+
+  return output
--- main.py
+++ main.py
@@ -5,25 +5,6 @@ import cv2
 from harris import harris
 from susan import susan
 
-def real_harris(img:np.ndarray):
-  """Returns a harris corner detection filter using OpenCV
-
-  Parameters
-  ----------
-  img : ndarray
-    Image array to detect corners
-
-  Returns
-  --------
-  dest : ndarray
-    An ndarray that has float numbers for all pixels in the array. If the number related to a pixel is positive, it is an edge. If negative, it is a corner.
-  """
-  operatedImage = np.float32(img)
-
-  dest:np.ndarray = cv2.cornerHarris(operatedImage, 2, 5, 0.07)
-  dest = cv2.dilate(dest, None)
-  return dest
-
 def filter_image(arr:np.ndarray, org:np.ndarray=[], title:str='Image'):
   """Filters given image array (has to be numpy array) and prints it
 
@@ -48,11 +29,8 @@ if __name__ == '__main__':
   img = Image.open('images/image_1.jpg').convert('L')
   # make the image numpy array
   img_arr = np.asarray(img)
-  
-  # get correct harris filter and print it to check our results
-  r_harris = real_harris(img_arr)
-  filter_image(r_harris, org=img_arr , title='Real Harris Detection')
 
-  # TODO: implement harris and SUSAN algorithms
-  h = harris(img_arr)
+  # TODO: implement SUSAN algorithm
+  h = harris(img_arr, 0.07, 2)
+  filter_image(h)
   s = susan(img_arr)
