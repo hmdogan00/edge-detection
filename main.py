84520f1a0c7f2e9fd7cb83471000f0b56d9d4938
hmdogan00 <58589080+hmdogan00@users.noreply.github.com>
2022-03-21 22:33:28 +0300

    format change

--- main.py
+++ main.py
@@ -5,14 +5,37 @@ import cv2
 from harris import harris
 from susan import susan
 
-def real_harris(img):
+def real_harris(img:np.ndarray):
+  """Returns a harris corner detection filter using OpenCV
+
+  Parameters
+  ----------
+  img : ndarray
+    Image array to detect corners
+
+  Returns
+  --------
+  dest : ndarray
+    An ndarray that has float numbers for all pixels in the array. If the number related to a pixel is positive, it is an edge. If negative, it is a corner.
+  """
   operatedImage = np.float32(img)
 
-  dest = cv2.cornerHarris(operatedImage, 2, 5, 0.07)
+  dest:np.ndarray = cv2.cornerHarris(operatedImage, 2, 5, 0.07)
   dest = cv2.dilate(dest, None)
   return dest
 
-def print_image(arr, org=[], title='Image'):
+def filter_image(arr:np.ndarray, org:np.ndarray=[], title:str='Image'):
+  """Filters given image array (has to be numpy array) and prints it
+
+  Parameters
+  ----------
+  arr : ndarray 
+    Filter array
+  org : ndarray 
+    Original array to be filtered - defaults to black image
+  title : str 
+    Title of the printed image
+  """
   if len(org) == 0:
     org = np.zeros(arr.shape)
   im = org[:,:]
@@ -28,7 +51,7 @@ if __name__ == '__main__':
   
   # get correct harris filter and print it to check our results
   r_harris = real_harris(img_arr)
-  print_image(r_harris, org=img_arr , title='Real Harris Detection')
+  filter_image(r_harris, org=img_arr , title='Real Harris Detection')
 
   # TODO: implement harris and SUSAN algorithms
   h = harris(img_arr)
