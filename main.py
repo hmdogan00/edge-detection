425d997632ea82fce3b6b2e09f9062f81eed7735
hmdogan00 <58589080+hmdogan00@users.noreply.github.com>
2022-03-27 18:16:38 +0300

    better functions

--- main.py
+++ main.py
@@ -5,10 +5,36 @@ from typing import Literal
 from harris import harris
 from susan import susan
 from sift import sift
+from datetime import datetime
 #delete later
-import ftdetect.features
+#import ftdetect.features
 import matplotlib.pyplot as plt
 #######################
+def timer(im, function, *args):
+  start = datetime.now()
+  function(im, *args)
+  end = datetime.now()
+  return end - start
+
+def test_stability(img1, img2, susan_hypers = [3, 27, 14.5], harris_hypers = [0.07, 5]):
+  sus1 = susan(img1, *susan_hypers)
+  sus2 = susan(img2, *susan_hypers)
+  har1 = harris(img1, *harris_hypers)
+  har2 = harris(img2, *harris_hypers)
+  print("stab_factor is",calculate_stability(sus1, sus2))
+  # calculate stability factor of image_4 and image_5 on harris
+  h1 = np.zeros(har1.shape)
+  h1[har1 > 0.01 * har1.max()] = 1
+  h2 = np.zeros(har2.shape)
+  h2[har2 > 0.01 * har2.max()] = 1
+  print("stab_factor is", calculate_stability(h1, h2))
+
+def calculate_stability(arr1, arr2):
+  intersect= np.count_nonzero(np.logical_and(arr1,arr2))
+  elem1 = np.count_nonzero(arr1)
+  elem2 = np.count_nonzero(arr2)
+  min_elem= min(elem1, elem2)
+  return (intersect / min_elem) * 100
 
 def filter_image(arr: np.ndarray, org: np.ndarray=[], title: str='Image', type: Literal['har', 'sus', 'sif'] = 'har'):
   """Filters given image array (has to be numpy array) and prints it
@@ -50,11 +76,11 @@ if __name__ == '__main__':
 
   #noise_dino1 = Image.open('images/noise_dino_1.png').convert('L')
   #noise_dino_arr1 = np.asarray(noise_dino1)
-
-  sus4 = susan(img_arr4, 3, 27, 14.5)
-  filter_image(sus4, img_arr4, type='sus')
-  sus5 = susan(img_arr5, 3, 27, 14.5)
-  filter_image(sus5, img_arr5, type='sus')
+  print(timer(img_arr4, susan, 3, 27, 14.5))
+  
+  #filter_image(sus4, img_arr4, type='sus')
+  #sus5 = susan(img_arr5, 3, 27, 14.5)
+  #filter_image(sus5, img_arr5, type='sus')
 
   #sus1 = susan(dino_arr1, 3, 27, 14.5)
   #filter_image(sus1, dino_arr1, type='sus')
@@ -67,9 +93,9 @@ if __name__ == '__main__':
 
 
   h4= harris(img_arr4, 0.07, 5)
-  filter_image(h4, img_arr4)
+  #filter_image(h4, img_arr4)
   h5 = harris(img_arr5, 0.07, 5)
-  filter_image(h5, img_arr5)
+  #filter_image(h5, img_arr5)
 
   #h1= harris(dino_arr1, 0.07, 5)
   #filter_image(h1, dino_arr1)
@@ -80,27 +106,5 @@ if __name__ == '__main__':
   #filter_image(h_noise_1, noise_dino_arr1)
 
   #calculate stability factor of image_4 and image_5 on susan
-  intersect= len(np.logical_and(sus4,sus5))
-  elem_sus4 = np.count_nonzero(sus4)
-  print('1',elem_sus4)
-  print('2 ', len(elem_sus4)*len(elem_sus4[0]))
-  elem_sus5 = np.count_nonzero(sus5)
-  min_elem= min(elem_sus4,elem_sus5)
-  stab_fact = (intersect / min_elem) * 100
-  print("stab_factor is",stab_fact)
-  # calculate stability factor of image_4 and image_5 on harris
-  h4_arr = np.zeros(h4.shape)
-  h4_arr[h4 > 0.01 * h4.max()] = 1
-  h5_arr = np.zeros(h5.shape)
-  h5_arr[h5 > 0.01 * h5.max()] = 1
-  intersect2 = len(np.logical_and(h4_arr,h5_arr))
-  elem_h4 = np.count_nonzero(h4_arr)
-  elem_h5 = np.count_nonzero(h5_arr)
-  min_elemh = min(elem_h4, elem_h5)
-  stab_facth = (intersect2/ min_elemh)
-  print("stab_factor is", stab_facth)
-
-
-
-
+  test_stability(img_arr4, img_arr5)
   #sif = sift(img_arr)
